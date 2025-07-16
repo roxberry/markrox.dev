a61c2d65b329429fd97f5cc821a971ddf1d7a078
Mark Roxberry <mark@driveapplied.com>
2025-07-15 21:29:49 -0400

    feat: enhance GoogleAdBanner functionality and update ProjectsPage layout with new project cards

--- src/components/GoogleAdBanner.jsx
+++ src/components/GoogleAdBanner.jsx
@@ -1,25 +1,42 @@
 // src/components/AdBanner.js
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-const GoogleAdBanner = () => {
+
+const GoogleAdBanner = ({ sidebarVisible }) => {
+    const [showAd, setShowAd] = useState(true);
+
     useEffect(() => {
-        try {
-            if (typeof window !== "undefined" && window.adsbygoogle) {
-                (window.adsbygoogle = window.adsbygoogle || []).push({});
-            }
-        } catch (e) {
-            console.error("Adsbygoogle error:", e);
-        }
-    }, []);
+        setShowAd(false);
+        const timeout = setTimeout(() => setShowAd(true), 100);
+        return () => clearTimeout(timeout);
+    }, [sidebarVisible]);
 
-    return (
-        <ins className="adsbygoogle"
-            style={{ display: "block"}}
+    useEffect(() => {
+        if (showAd) {
+            try {
+                if (typeof window !== "undefined" && window.adsbygoogle) {
+                    (window.adsbygoogle = window.adsbygoogle || []).push({});
+                }
+            } catch (e) {
+                console.error("Adsbygoogle error:", e);
+            }
+        }
+    }, [showAd]);
+
+    return showAd ? (
+        <ins
+            className="adsbygoogle"
+            style={{
+                display: "block",
+                maxWidth: "870px",
+                margin: sidebarVisible ? "0 auto" : undefined
+            }}
             data-ad-client="ca-pub-3997623664966064"
             data-ad-slot="8711602606"
             data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-    );
+            data-full-width-responsive="true"
+        ></ins>
+    ) : null;
 };
 
 export default GoogleAdBanner;
