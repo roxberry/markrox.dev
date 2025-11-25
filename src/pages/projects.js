95f69478c88d972fc0b47831b5d05dbe979f8ad3
Mark Roxberry <mark@driveapplied.com>
2025-11-25 17:58:26 -0500

    externalized projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Projects from "../components/Projects"
 import Seo from "../components/Seo"
+
 import { StaticImage } from "gatsby-plugin-image"
 
 const ProjectsPage = () => {
@@ -9,50 +11,7 @@ const ProjectsPage = () => {
             <Seo title="Projects" />
             <section>
                 <h1 className="sectionTitle">Projects</h1>
-                <div className="flexbox-wide">
-                    <div className="project-card-wide">
-                        <div className="project-card-image">
-                            <StaticImage
-                                formats={["AUTO"]}
-                                src="../images/projects/hactu8.jpg"
-                                alt="project"
-                                placeholder="NONE"
-                                background="#282828"
-                                loading="eager"
-                                layout="constrained"
-                                width={250}
-                            />
-                        </div>
-                        <div className="project-card-wide-text">
-                            <h3>HACTU8.COM</h3>
-                            <span>A cutting-edge initiative dedicated to ethical hacking in the domains of robotics, AI, IoT, and consumer hardware.</span>
-                            <div className="project-card-link">
-                                <a href="https://hactu8.com" target="_blank" rel="noreferrer">Visit the project &gt;</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="project-card-wide">
-                        <div className="project-card-image">
-                            <StaticImage
-                                formats={["AUTO"]}
-                                src="../images/projects/briefing-logo.webp"
-                                alt="project"
-                                placeholder="NONE"
-                                background="#282828"
-                                loading="eager"
-                                layout="constrained"
-                                width={250}
-                            />
-                        </div>
-                        <div className="project-card-wide-text">
-                            <h3>Briefing</h3>
-                            <span>A knowledge tool designed to help professionals master knowledge by delivering curated, topic-specific content.</span>
-                            <div className="project-card-link">
-                                <a href="https://briefing.driveapplied.com" target="_blank" rel="noreferrer">Visit the project &gt;</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Projects />
             </section>
         </Layout>
     )
--- src/templates/posts.js
+++ src/templates/posts.js
@@ -5,6 +5,7 @@ import AmazonSiteStripe from "../components/AmazonSiteStripe"
 import amazonProducts from "../config/amazonProducts"
 import Pager from "../components/Pager"
 import TagList from "../components/TagList"
+import Projects from "../components/Projects"
 import { StaticImage, GatsbyImage } from "gatsby-plugin-image"
 import { Link, graphql } from "gatsby"
 
@@ -183,50 +184,7 @@ class BlogIndex extends React.Component {
                 </section>
                 <section>
                     <h1 className="sectionTitle">Featured Projects</h1>
-                    <div className="flexbox-wide">
-                        <div className="project-card-wide">
-                            <div className="project-card-image">
-                                <StaticImage
-                                    formats={["AUTO"]}
-                                    src="../images/projects/hactu8.jpg"
-                                    alt="project"
-                                    placeholder="NONE"
-                                    background="#282828"
-                                    loading="eager"
-                                    layout="constrained"
-                                    width={250}
-                                />
-                            </div>
-                            <div className="project-card-wide-text">
-                                <h3>HACTU8.COM</h3>
-                                <span>A cutting-edge initiative dedicated to ethical hacking in the domains of robotics, AI, IoT, and consumer hardware.</span>
-                                <div className="project-card-link">
-                                    <a href="https://hactu8.com" target="_blank" rel="noreferrer">Visit the project &gt;</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="project-card-wide">
-                            <div className="project-card-image">
-                                <StaticImage
-                                    formats={["AUTO"]}
-                                    src="../images/projects/briefing-logo.webp"
-                                    alt="project"
-                                    placeholder="NONE"
-                                    background="#282828"
-                                    loading="eager"
-                                    layout="constrained"
-                                    width={250}
-                                />
-                            </div>
-                            <div className="project-card-wide-text">
-                                <h3>Briefing</h3>
-                                <span>A knowledge tool designed to help professionals master knowledge by delivering curated, topic-specific content.</span>
-                                <div className="project-card-link">
-                                    <a href="https://briefing.driveapplied.com" target="_blank" rel="noreferrer">Visit the project &gt;</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Projects />
                 </section>
             </Layout>
         )
